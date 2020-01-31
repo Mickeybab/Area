68b71b41120c0420c4fcdef8d1c380b88ad8fadd
Julien Ollivier <33491266+juulieen@users.noreply.github.com>
2020-01-31 15:01:18 +0100

    [RMV] useless comment

--- area_front/lib/backend/Navigation.dart
+++ area_front/lib/backend/Navigation.dart
@@ -1,8 +1,6 @@
 // Core
 import 'package:flutter/material.dart';
 
-// Model
-
 class Navigation {
   static navigate(BuildContext context, String routeName) {
     Navigator.of(context).pushReplacementNamed(routeName);
