60abaa170255bb847158cd0f096ba2eed75636e8
Julien Ollivier <33491266+juulieen@users.noreply.github.com>
2020-02-11 10:43:46 +0100

    [RMV] useless import

--- area_front/lib/pages/Home.dart
+++ area_front/lib/pages/Home.dart
@@ -1,18 +1,13 @@
 // Core
-import 'package:area_front/backend/Navigation.dart';
-import 'package:area_front/widgets/AreaLargeButton.dart';
-import 'package:area_front/widgets/AreaTitle.dart';
 import 'package:area_front/widgets/GetMore.dart';
 import 'package:flutter/material.dart';
 
 // Constants
-import 'package:area_front/static/Constants.dart';
 
 // My Widgets
 import 'package:area_front/widgets/topbar/TopBar.dart';
 
 // Datas
-import 'package:area_front/static/Routes.dart';
 
 /// `Home` Page of the Area Project
 class HomePage extends StatelessWidget {
