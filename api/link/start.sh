c37ffc8d7e23612656f69e7179f0431fd3082ba3
Enzo PERROT <enzo.perrot@ouisnap.fr>
2020-02-17 09:57:40 +0100

    [ADD] default admin user "admin admin"

--- api/link/start.sh
+++ api/link/start.sh
@@ -7,4 +7,6 @@ done
 
 python3 manage.py migrate
 
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'bleemoe@bleemoe.com', 'admin')" | python3 manage.py shell 2> /dev/null
+
 python3 -u manage.py runserver --verbosity 3 0.0.0.0:9000
