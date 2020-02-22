3c288f839b71b58bccbc10fa77b233577b7ab4a9
Florent Poinsard <florent.poinsard@outlook.fr>
2020-02-22 14:08:31 +0100

    [ADD] - get last commit on api github

--- api/github/models/models.js
+++ api/github/models/models.js
@@ -1,8 +1,25 @@
 const Octokit = require("@octokit/rest");
 
 module.exports = {
+    getLastCommit,
     getLastIssueOfRepo,
     getLastPullOfRepo
+}
+
+function getLastCommit(auth, owner, repo) {
+    const clientWithAuth = new Octokit({
+        auth: "token " + auth
+    })
+
+    return new Promise(function (resolve, reject) {
+        clientWithAuth.repos.listCommits({ owner: owner, repo: repo }).then(res => {
+            console.log(res.data[1].commit)
+            return resolve(res.data)
+        }).catch((err) => setImmediate(() => {
+            console.log(err)
+            return reject(err)
+        }))
+    })
 }
 
 function getLastIssueOfRepo(auth, owner, repo) {
