5b76c57358352b11764feb97281ab3112e72ece1
Julien Ollivier <33491266+juulieen@users.noreply.github.com>
2020-02-26 17:21:28 +0100

    [FIX] Add float to accepted format

--- area_front/lib/pages/Applet.dart
+++ area_front/lib/pages/Applet.dart
@@ -13,6 +13,8 @@ import 'package:area_front/models/applets/Applet.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
+import 'dart:math' as math;
+
 class AppletsDetailsPage extends StatefulWidget {
   const AppletsDetailsPage(this.applet, {Key key}) : super(key: key);
 
@@ -230,11 +232,11 @@ class ReactionParam extends StatelessWidget {
   Widget build(BuildContext context) {
     final textInputType = (widget.applet.reaction.param[index].type == "string")
         ? TextInputType.text
-        : TextInputType.number;
+        : TextInputType.numberWithOptions(decimal: true);
     final textInputFormatter =
         (widget.applet.reaction.param[index].type == "string")
             ? BlacklistingTextInputFormatter.singleLineFormatter
-            : WhitelistingTextInputFormatter.digitsOnly;
+            : DecimalTextInputFormatter(decimalRange: 2);
     print(widget.applet.reaction.param[index].name);
     return Row(
       children: <Widget>[
@@ -256,5 +258,45 @@ class ReactionParam extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class DecimalTextInputFormatter extends TextInputFormatter {
+  DecimalTextInputFormatter({this.decimalRange})
+      : assert(decimalRange == null || decimalRange > 0);
+
+  final int decimalRange;
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue, // unused.
+    TextEditingValue newValue,
+  ) {
+    TextSelection newSelection = newValue.selection;
+    String truncated = newValue.text;
+
+    if (decimalRange != null) {
+      String value = newValue.text;
+
+      if (value.contains(".") &&
+          value.substring(value.indexOf(".") + 1).length > decimalRange) {
+        truncated = oldValue.text;
+        newSelection = oldValue.selection;
+      } else if (value == ".") {
+        truncated = "0.";
+
+        newSelection = newValue.selection.copyWith(
+          baseOffset: math.min(truncated.length, truncated.length + 1),
+          extentOffset: math.min(truncated.length, truncated.length + 1),
+        );
+      }
+
+      return TextEditingValue(
+        text: truncated,
+        selection: newSelection,
+        composing: TextRange.empty,
+      );
+    }
+    return newValue;
   }
 }
