88492d4cc9d4fcf497fe19f1e4727d441adbae84
Cecile <cecile.cadoul@epitech.eu>
2020-02-28 23:52:10 +0100

    [ADD] Epitech Sync back Button handler

--- area_front/lib/pages/services/EpitechNoSyncService.dart
+++ area_front/lib/pages/services/EpitechNoSyncService.dart
@@ -73,7 +73,7 @@ class _EpitechNoSyncServicePageState extends State<EpitechNoSyncServicePage> {
               ),
             );
           } catch (e) {
-            setState(() => error = e.message);
+            setState(() => error = e);
           }
         }
       },
